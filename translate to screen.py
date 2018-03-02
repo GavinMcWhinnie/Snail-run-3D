8abd8922e02e5acac287cc45ad5c44811d3b6213
Gavin McWhinnie <gavinthegav@gmail.com>
2018-03-02 10:30:36 +0000

    Changed to a function
    returns 2D point on screen

--- translate to screen.py
+++ translate to screen.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 
 #inputs:
@@ -7,98 +6,108 @@ import numpy as np
 #viewer distance
 #3D point
 
-screen_position = np.array([100,100,100])
-screen_direction = np.array([-1,-1,-1])
-screen_rotation = 0
-viewer_distance = 50
-_3D_point = np.array([0,0,100])
+#screen_position = np.array([100,100,100])
+#screen_direction = np.array([-1,-1,-1])
+#viewer_distance = 50
+#_3D_point = np.array([0,0,100])
 
-###### step 1 ######
-#work out where viewer is
+def translate(_3D_point, screen_position, screen_direction, viewer_distance):
 
-magnitude_of_direction = np.sqrt(screen_direction.dot(screen_direction))
-viewer_vector = screen_direction * (viewer_distance / -magnitude_of_direction)
+    ## screen rotation is not yet available
+    ## prints error if point not visible - what else should happen instead?
+    
+    ###### step 1 ######
+    #work out where viewer is
 
-viewer_position = viewer_vector + screen_position
+    magnitude_of_direction = np.sqrt(screen_direction.dot(screen_direction))
+    viewer_vector = screen_direction * (viewer_distance / -magnitude_of_direction)
 
-###### step 2 ######
-#create line from point to viewer
+    viewer_position = viewer_vector + screen_position
 
-point_to_viewer = viewer_position - _3D_point
+    ###### step 2 ######
+    #create line from point to viewer
 
-
-###### step 3 ######
-#create plane that screen rests on
-
-a, b, c = screen_direction
-
-d = -(screen_direction.dot(screen_position))
-
-viewer_side_of_plane = abs(viewer_position.dot(screen_direction) + d) / (viewer_position.dot(screen_direction) + d)
-
-if (_3D_point.dot(screen_direction) + d) * viewer_side_of_plane == abs(_3D_point.dot(screen_direction) + d):
-    print("ERROR: point not visible")
-    ######### point cannot be shown as it is on the same side of screen as viewer ########
-else:
-    pass
-    ##point is visible
-
-###### step 4 ######
-#find out where line intersects the screen
-
-#ax + by + cz + d = 0
-#a(p + td) + b(q + te) + c(r + tf) + d = 0
-#ap + atd + bq + bte + cr + ctf + d = 0
-#ap + bq + cr + d = -atd - bte - ctf = 0
-#-t (ad + be + cf) = ap + bq + cr + d
-#t = -(ap + bq + cr + d)/(ad + be + cf)
-#t = -(a, b, c) . (p, q, r) + d / (a, b, c) . (d, e, f)
-
-t = -((screen_direction.dot(_3D_point) + d) / screen_direction.dot(point_to_viewer))
-
-intersection = _3D_point + (point_to_viewer * t)
+    point_to_viewer = viewer_position - _3D_point
 
 
-###### step 5 ######
+    ###### step 3 ######
+    #create plane that screen rests on
 
-ratio_for_x_axis = 1 / np.sqrt(a**2 + b**2 + c**2)
+    a, b, c = screen_direction
 
-x_axis = np.array([-b * ratio_for_x_axis, a * ratio_for_x_axis, 0])
+    d = -(screen_direction.dot(screen_position))
 
-#ax + by + cz = 0
-#-cz = ax + by
-#z = -(ax + by)/c
+    viewer_side_of_plane = abs(viewer_position.dot(screen_direction) + d) / (viewer_position.dot(screen_direction) + d)
 
-p = -((abs(c)/c) * a)
-q = -((abs(c)/c) * b)
-r = -(a * p + b * q)/c
+    if (_3D_point.dot(screen_direction) + d) * viewer_side_of_plane == abs(_3D_point.dot(screen_direction) + d):
+        print("ERROR: point not visible")
+        ######### point cannot be shown as it is on the same side of screen as viewer ########
+    else:
+        pass
+        ##point is visible
 
-y_axis = np.array([p, q, r])
+    ###### step 4 ######
+    #find out where line intersects the screen
 
-magnitude_of_y_axis = np.sqrt(y_axis.dot(y_axis))
-ratio_for_y_axis = 1 / magnitude_of_y_axis
+    #ax + by + cz + d = 0
+    #a(p + td) + b(q + te) + c(r + tf) + d = 0
+    #ap + atd + bq + bte + cr + ctf + d = 0
+    #ap + bq + cr + d = -atd - bte - ctf = 0
+    #-t (ad + be + cf) = ap + bq + cr + d
+    #t = -(ap + bq + cr + d)/(ad + be + cf)
+    #t = -(a, b, c) . (p, q, r) + d / (a, b, c) . (d, e, f)
 
-y_axis = y_axis * ratio_for_y_axis
+    t = -((screen_direction.dot(_3D_point) + d) / screen_direction.dot(point_to_viewer))
 
-###### step 6 ######
-
-vector_on_plane = intersection - screen_position
-
-#vector_on_plane = [l, m, n]
-#x_axis = [a, b, 0]
-#y_axis = [d, e, f]
-#camera_position = [p, q, r]
-
-#[l]   [xa]   [yd]
-#[m] = [xb] + [ye]
-#[n]   [0 ]   [yf]
-
-y = vector_on_plane[2] / y_axis[2]
-
-x = (vector_on_plane[0] - y * y_axis[0]) / x_axis[0]
-
-_2D_vector = np.array([x, y])
-
-print(_2D_vector)
+    intersection = _3D_point + (point_to_viewer * t)
 
 
+    ###### step 5 ######
+
+    ratio_for_x_axis = 1 / np.sqrt(a**2 + b**2 + c**2)
+
+    x_axis = np.array([-b * ratio_for_x_axis, a * ratio_for_x_axis, 0])
+
+    #ax + by + cz = 0
+    #-cz = ax + by
+    #z = -(ax + by)/c
+
+    p = -((abs(c)/c) * a)
+    q = -((abs(c)/c) * b)
+    r = -(a * p + b * q)/c
+
+    y_axis = np.array([p, q, r])
+
+    magnitude_of_y_axis = np.sqrt(y_axis.dot(y_axis))
+    ratio_for_y_axis = 1 / magnitude_of_y_axis
+
+    y_axis = y_axis * ratio_for_y_axis
+
+    ###### step 6 ######
+
+    vector_on_plane = intersection - screen_position
+
+    #vector_on_plane = [l, m, n]
+    #x_axis = [a, b, 0]
+    #y_axis = [d, e, f]
+    #camera_position = [p, q, r]
+
+    #[l]   [xa]   [yd]
+    #[m] = [xb] + [ye]
+    #[n]   [0 ]   [yf]
+
+    y = vector_on_plane[2] / y_axis[2]
+
+    x = (vector_on_plane[0] - y * y_axis[0]) / x_axis[0]
+
+    _2D_vector = np.array([x, y])
+
+    return _2D_vector
+
+if __name__ == "__main__":
+    screen_position = np.array([100,100,100])
+    screen_direction = np.array([-1,-1,-1])
+    viewer_distance = 50
+    _3D_point = np.array([0,0,100])
+
+    print(translate(_3D_point, screen_position, screen_direction, viewer_distance))
